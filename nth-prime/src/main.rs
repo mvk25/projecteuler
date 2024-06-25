425cf9278c82ca2f1e3a9e676f66c7cd582c11a6
mvk25 <lizhawai@gmail.com>
2024-06-25 08:26:02 +0300

    10001st prime

--- nth-prime/src/main.rs
+++ nth-prime/src/main.rs
@@ -1,21 +1,44 @@
-fn isprime(num: u32) -> bool {
-    (2..=num).all(|x| num % x != 0)
+fn isprime(num: u32, primes: &Vec<u32>) -> bool {
+    if num < 2 {
+        return false;
+    }
+
+    let sqrt = (num as f64).sqrt() as u32;
+    for &prime in primes.iter() {
+        if prime > sqrt {
+            break;
+        }
+        if num % prime == 0 {
+            return false;
+        }
+    }
+    true
 }
 
-fn nth_prime(pos: u32) {
-    let mut count = 0;
+fn nth_prime(pos: u32) -> u32 {
+    if pos == 1 {
+        return 2;
+    }
+
+    let mut primes = vec![2];
+    let mut count = 1;
     let mut i = 3;
-    loop {
-        if isprime(i) { count += 1; }
-        if count == pos { break; }
+
+    while count < pos {
+        if isprime(i, &primes) {
+            primes.push(i);
+            count += 1;
+        }
+
         i += 2;
     }
 
-    println!("{}", i);
+    primes[(pos - 1) as usize]
 }
 
 fn main() {
-    nth_prime(10001);
+    let nth = nth_prime(10001);
+    println!("{}", nth);
 }
 
 
